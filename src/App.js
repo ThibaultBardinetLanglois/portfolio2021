d54621de46fd43585bd1385d464f3b3a13dbda33
thib <langlois.bardinet.thibault@gmail.com>
2021-11-21 05:11:53 +0100

    Add HashRouter instead BrowserRouter

--- src/App.js
+++ src/App.js
@@ -47,19 +47,19 @@ function App() {
       <div className="app">
         {loading ? <Loader />
         :
-          <div>
-        <div className="burger-menu">
-          <div className="top-line line"></div>
-          <div className="center-line line"></div>
-          <div className="bottom-line line"></div>
-        </div>
-        <Switch>
-            <Route exact path="/" component={Home}/>
-            <Route exact path="/about" component={About}/>
-            <Route exact path="/work" component={Work}/>
-            <Route exact path="/contact" component={Contact}/>
-            <Redirect to="/"/>
-        </Switch>
+        <div>
+          <div className="burger-menu">
+            <div className="top-line line"></div>
+            <div className="center-line line"></div>
+            <div className="bottom-line line"></div>
+          </div>
+          <Switch>
+              <Route exact path="/" component={Home}/>
+              <Route exact path="/about" component={About}/>
+              <Route exact path="/work" component={Work}/>
+              <Route exact path="/contact" component={Contact}/>
+              <Redirect to="/"/>
+          </Switch>
         </div>
       }
       </div>
